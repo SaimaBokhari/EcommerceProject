662788a45784b2741dc6248b5ee0200744ded24d
SaimaBokhari <ssbokhari14@gmail.com>
2023-01-24 22:57:44 +0400

    first commit

---
+++ src/test/java/allovercommerce/pages/HomePage.java
@@ -0,0 +1,16 @@
+package allovercommerce.pages;
+
+import allovercommerce.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    public HomePage(){  // Constructor
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    // Locate elements by using any locator
+
+
+
+}
---
+++ src/test/java/allovercommerce/pages/MyAccountPage.java
@@ -0,0 +1,16 @@
+package allovercommerce.pages;
+
+import allovercommerce.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class MyAccountPage {
+
+    public MyAccountPage(){  // Constructor
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    // Locate elements by using any locator
+
+
+
+}
