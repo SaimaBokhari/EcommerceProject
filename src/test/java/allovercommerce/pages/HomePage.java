e0409b1c31f28a7663485a52b6cd45b5123d19f2
SaimaBokhari <ssbokhari14@gmail.com>
2023-01-29 17:30:13 +0400

    first commit from saima

--- src/test/java/allovercommerce/pages/HomePage.java
+++ src/test/java/allovercommerce/pages/HomePage.java
@@ -1,6 +1,8 @@
 package allovercommerce.pages;
 
 import allovercommerce.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
@@ -9,8 +11,65 @@ public class HomePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    // Locate elements by using any locator
+    // Locate elements for signing up/registration
 
+    @FindBy(xpath = "//a[.='Register']")
+    public WebElement registerOption;
+
+    @FindBy(id = "reg_username")
+    public WebElement reg_username;
+
+    @FindBy(id = "reg_email")
+    public WebElement reg_email;
+
+    @FindBy(id = "reg_password")
+    public WebElement reg_password;
+
+    @FindBy(xpath = "//a[@class='register_as_vendor']")
+    public WebElement vendorSignupOption;
+
+    @FindBy(id = "register-policy")
+    public WebElement registerPolicy;
+
+    @FindBy(name = "register")
+    public WebElement signupButton;
+
+    @FindBy(xpath = "//a[.='Sign Out']")
+    public WebElement signOutButton;
+
+    @FindBy(xpath = "//a[.='Log out']")
+    public WebElement confirmLogOutButton;
+
+    @FindBy(xpath = "(//p[@class='submit-status'])[2]")
+    public WebElement accountAlreadyExistMessage;
+
+
+
+    // Locate elements for "BECOME A VENDOR"
+
+    @FindBy(linkText = "Become a Vendor")
+    public WebElement becomeAVendorOption;
+
+
+    // Locate elements for SIGN IN
+
+    @FindBy(partialLinkText = "Sign In")
+    public WebElement signInButton;
+
+    @FindBy(id = "username")
+    public WebElement userName;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "rememberme")
+    public WebElement rememberMeCheckbox;
+
+    @FindBy(xpath = "(//button[@type='submit'])[2]")
+    public WebElement signInSubmitButton;
+
+    @FindBy(xpath = "(//i[@class='w-icon-account'])[1]")
+    public WebElement userIcon;
 
 
 }
